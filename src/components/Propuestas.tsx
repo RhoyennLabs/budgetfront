42008219e72b1af3accde9042aec3dfc054f6cd8
Rodrigo <rodrigomreidenbach@gmail.com>
2025-05-31 20:21:45 -0400

    puxin

--- src/components/Propuestas.tsx
+++ src/components/Propuestas.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import ModalPropuestas from "./ModalPropuestas";
 import ModalDetalleMes from "./ModalDetalleMes";
 import axios from "axios";
